7840c19eb71a22d95cf5ee4269acbba19007f935
PijusSerapinasMipsas <pijus.s@mipsas.com>
2023-12-17 21:55:56 +0200

    auth0 didn't work out

--- src/handlers/rest/index.ts
+++ src/handlers/rest/index.ts
@@ -1,30 +1,15 @@
 import { PrismaClient } from '@prisma/client'
 import { Express } from 'express'
-import { auth } from 'express-openid-connect'
-
-const config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: process.env.AUTH0_SECRET,
-    baseURL: process.env.BASE_URL,
-    clientID: process.env.CLIENT_ID,
-    issuerBaseURL: process.env.ISSUER_BASE_URL,
-}
 
 export const setupRestHandlers = (app: Express, prisma: PrismaClient) => {
-    app.use(auth(config))
-
-    app.get('/', (req, res) => {
+    app.get('/', (_, res) => {
         res.send({
             message: 'Welcome to the OSS API',
-            auth: req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out',
         })
     })
 
-    app.get('/spaces', async (req, res) => {
-        const spaces = req.oidc.isAuthenticated()
-            ? await prisma.sportSpace.findMany()
-            : []
+    app.get('/spaces', async (_, res) => {
+        const spaces = await prisma.sportSpace.findMany()
         res.send(spaces)
     })
 }
