1692f5b235d2af266269d1677616781bab03865a
PijusSerapinasMipsas <pijus.s@mipsas.com>
2023-12-19 22:10:03 +0200

    added favorites and logger

--- src/handlers/rest/index.ts
+++ src/handlers/rest/index.ts
@@ -3,8 +3,31 @@ import { Express } from 'express'
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import { verifyToken } from '../../middleware/auth'
+import winston from 'winston'
+
+const logger = winston.createLogger({
+    // Log only if level is less than (meaning more severe) or equal to this
+    level: 'info',
+    // Use timestamp and printf to create a standard log format
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(
+            (info) => `${info.timestamp} ${info.level}: ${info.message}`
+        )
+    ),
+    // Log to the console and a file
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'logs/app.log' }),
+    ],
+})
 
 export const setupRestHandlers = (app: Express, prisma: PrismaClient) => {
+    app.use((req, _, next) => {
+        logger.info(`${req.method} request for ${req.url}`)
+        next()
+    })
+
     app.get('/', (_, res) => {
         res.send({
             message: 'Welcome to the OSS API',
@@ -16,12 +39,59 @@ export const setupRestHandlers = (app: Express, prisma: PrismaClient) => {
         res.send(spaces)
     })
 
+    app.patch('/add-favorite', verifyToken, async (req: any, res) => {
+        const { spaceId } = req.body
+
+        const user = await prisma.user.findFirst({
+            where: {
+                id: req.user.user_id,
+            },
+        })
+
+        if (user) {
+            const { favorites } = user
+
+            if (!(favorites as string[])!.includes(spaceId)) {
+                const newFavorites = [...(favorites as string[]), spaceId]
+                const updatedUser = await prisma.user.update({
+                    where: {
+                        id: req.user.user_id,
+                    },
+                    data: {
+                        favorites: newFavorites,
+                    },
+                })
+                return res.status(200).send(updatedUser)
+            } else {
+                const newFavorites = (favorites as string[]).filter(
+                    (id) => id !== spaceId
+                )
+                const updatedUser = await prisma.user.update({
+                    where: {
+                        id: req.user.user_id,
+                    },
+                    data: {
+                        favorites: newFavorites,
+                    },
+                })
+
+                return res.status(200).send(updatedUser)
+            }
+        }
+
+        return res.status(400).send({
+            error: 'USER_NOT_FOUND',
+        })
+    })
+
     app.post('/register', async (req, res) => {
         try {
             const { name, email, password } = req.body
 
             if (!(email && password && name)) {
-                res.status(400).send('All input is required')
+                return res.status(400).send({
+                    error: 'ALL_INPUTS_REQUIRED',
+                })
             }
 
             const oldUser = await prisma.user.findFirst({
@@ -31,7 +101,9 @@ export const setupRestHandlers = (app: Express, prisma: PrismaClient) => {
             })
 
             if (oldUser) {
-                return res.status(409).send('User Already Exist. Please Login')
+                return res.status(409).send({
+                    error: 'USER_ALREADY_EXISTS',
+                })
             }
 
             const encryptedUserPassword = await bcrypt.hash(password, 10)
@@ -74,8 +146,10 @@ export const setupRestHandlers = (app: Express, prisma: PrismaClient) => {
             const { email, password } = req.body
 
             // Validate user input
-            if (!(email && password)) {
-                res.status(400).send('All input is required')
+            if (!email || !password) {
+                return res.status(400).send({
+                    error: 'ALL_INPUTS_REQUIRED',
+                })
             }
             // Validate if user exist in our database
             const user = await prisma.user.findFirst({ where: { email } })
@@ -104,7 +178,9 @@ export const setupRestHandlers = (app: Express, prisma: PrismaClient) => {
                 return res.status(200).json(updatedUser)
             }
         } catch (err) {
-            return res.status(400).send('Invalid Credentials')
+            return res.status(400).send({
+                error: 'INVALID_CREDENTIALS',
+            })
         }
     })
 }
