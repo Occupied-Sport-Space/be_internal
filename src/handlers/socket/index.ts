cf3e36eff071145a307be9364e643ae852369a40
PijusSerapinasMipsas <pijus.s@mipsas.com>
2023-12-28 19:54:02 +0200

    added need for auth token, to make count changes

--- src/handlers/socket/index.ts
+++ src/handlers/socket/index.ts
@@ -5,44 +5,53 @@ interface CountInstance {
     count: number
     spaceId: string
     cameraId: number
+    authToken: string
 }
 
 export const setupSocketHandlers = (io: Server, prisma: PrismaClient) => {
     io.on('connection', (socket) => {
         console.log('user connected!')
 
-        socket.on('count', ({ count, spaceId, cameraId }: CountInstance) => {
-            console.log('count received', count, spaceId, cameraId)
-            prisma.sportSpace
-                .findFirst({
-                    where: {
-                        id: spaceId,
-                    },
-                })
-                .then((space) => {
-                    if (space) {
-                        const { availability } = space
-                        prisma.sportSpace
-                            .update({
-                                where: {
-                                    id: spaceId,
-                                },
-                                data: {
-                                    availability: (
-                                        availability as number[]
-                                    ).map((num, i) =>
-                                        i !== cameraId ? num : count
-                                    ),
-                                },
-                            })
-                            .then((newSpace) => {
-                                socket.broadcast.emit('update', newSpace)
-                            })
-                    } else {
-                        console.log('Space not found')
-                    }
-                })
-        })
+        socket.on(
+            'count',
+            ({ count, spaceId, cameraId, authToken }: CountInstance) => {
+                if (authToken === process.env.TOKEN_KEY) {
+                    console.log('count received', count, spaceId, cameraId)
+                    prisma.sportSpace
+                        .findFirst({
+                            where: {
+                                id: spaceId,
+                            },
+                        })
+                        .then((space) => {
+                            if (space) {
+                                const { availability } = space
+                                prisma.sportSpace
+                                    .update({
+                                        where: {
+                                            id: spaceId,
+                                        },
+                                        data: {
+                                            availability: (
+                                                availability as number[]
+                                            ).map((num, i) =>
+                                                i !== cameraId ? num : count
+                                            ),
+                                        },
+                                    })
+                                    .then((newSpace) => {
+                                        socket.broadcast.emit(
+                                            'update',
+                                            newSpace
+                                        )
+                                    })
+                            } else {
+                                console.log('Space not found')
+                            }
+                        })
+                }
+            }
+        )
 
         socket.on('disconnect', () => {
             console.log('A user just disconnected')
